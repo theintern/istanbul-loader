444f4ff28162e5365c240316dcb8d54764d635c1
Jason Cheatham <j.cheatham@gmail.com>
2018-02-09 13:30:21 -0500

    Fix Loader API bug, update source-map dependency
    * Return source maps as objects rather than strings
    * Update source-map to 0.6.1. Don't use 0.7.0 yet to avoid type
      conflicts with the version of source-map used by
      istanbul-lib-instrument.
    
    Fix typings, use ts-node for tests, downgrade source-map

--- tests/loader.ts
+++ tests/loader.ts
@@ -1,31 +1,56 @@
-import { SinonSandbox, sandbox as Sandbox } from 'sinon';
+import { SinonSandbox, sandbox as Sandbox, SinonStub } from 'sinon';
+import { SourceMapGenerator, RawSourceMap } from 'source-map';
 import {
 	disable as disableMockery,
 	enable as enableMockery,
 	registerAllowable,
+	warnOnUnregistered,
 	registerMock,
 	deregisterMock
 } from 'mockery';
 
-const { after, afterEach, before, describe, it } = intern.getInterface('bdd');
+const { after, before, beforeEach, describe, it } = intern.getInterface('bdd');
 const { assert } = intern.getPlugin('chai');
 
-function getSourceMap() {
-	return {
-		sources: ['some/path!myFile.ts', 'myFile2.ts']
-	};
+function getSourceMap(webpackPaths = true) {
+	const generator = new SourceMapGenerator({
+		file: 'myFile.ts'
+	});
+	generator.addMapping({
+		source: webpackPaths ? 'some/path!myFile1.ts' : 'myFile1.ts',
+		generated: {
+			line: 5,
+			column: 20
+		},
+		original: {
+			line: 10,
+			column: 15
+		}
+	});
+	generator.addMapping({
+		source: 'myFile2.ts',
+		generated: {
+			line: 4,
+			column: 19
+		},
+		original: {
+			line: 9,
+			column: 14
+		}
+	});
+	return JSON.parse(generator.toString());
 }
 
 describe('istanbul-loader', () => {
 	let loaderUnderTest: any;
-	let instrumentMock: any;
-	let sourceMapMock: any;
+	let instrumentMock: SinonStub;
+	let sourceMapMock: SinonStub;
 	let sandbox: SinonSandbox;
 
 	before(() => {
 		sandbox = Sandbox.create();
 		instrumentMock = sandbox.stub().callsArg(2);
-		sourceMapMock = sandbox.stub().returns({});
+		sourceMapMock = sandbox.stub().returns(getSourceMap(false));
 
 		enableMockery({ useCleanCache: true });
 		registerMock('istanbul-lib-instrument', {
@@ -36,8 +61,14 @@ describe('istanbul-loader', () => {
 				};
 			}
 		});
-		registerAllowable('src/index');
-		loaderUnderTest = require('src/index').default;
+		registerMock('loader-utils', {
+			getOptions() {
+				return null;
+			}
+		});
+		registerAllowable('../src/index');
+		warnOnUnregistered(false);
+		loaderUnderTest = require('../src/index').default;
 	});
 
 	after(() => {
@@ -45,52 +76,62 @@ describe('istanbul-loader', () => {
 		disableMockery();
 	});
 
-	afterEach(() => {
+	beforeEach(() => {
 		sandbox.resetHistory();
 	});
 
+	function callLoader(sourceMap: RawSourceMap | null) {
+		return new Promise<{ source: string; sourceMap?: RawSourceMap }>(
+			(resolve, reject) => {
+				loaderUnderTest.call(
+					{
+						async: () => (
+							error: Error,
+							source: string,
+							sourceMap?: RawSourceMap
+						) => {
+							if (error) {
+								reject(error);
+							} else {
+								resolve({ source, sourceMap });
+							}
+						}
+					},
+					'content',
+					sourceMap
+				);
+			}
+		);
+	}
+
 	it('should call istanbul to instrument files', () => {
-		return new Promise(resolve => {
-			loaderUnderTest.call(
-				{ async: () => resolve },
-				'content',
-				getSourceMap()
-			);
-		}).then(() => {
+		return callLoader(getSourceMap()).then(() => {
 			assert.equal(instrumentMock.callCount, 1);
 		});
 	});
 
 	it('handles no source map', () => {
-		return new Promise(resolve => {
-			loaderUnderTest.call({ async: () => resolve }, 'content', null);
-		}).then(() => {
+		return callLoader(null).then(() => {
 			assert.equal(instrumentMock.callCount, 1);
 		});
 	});
 
 	it('handles a source map with no sources', () => {
-		return new Promise(resolve => {
-			loaderUnderTest.call({ async: () => resolve }, 'content', {});
-		}).then(() => {
+		const sourceMap = getSourceMap();
+		delete sourceMap.sources;
+		return callLoader(sourceMap).then(() => {
 			assert.equal(instrumentMock.callCount, 1);
 		});
 	});
 
-	it('should fix source maps', () => {
-		let sourceMap = getSourceMap();
-
-		sourceMapMock.reset();
-		sourceMapMock.returns(sourceMap);
-
-		return new Promise(resolve => {
-			loaderUnderTest.call(
-				{ async: () => resolve },
-				'content',
-				sourceMap
-			);
-		}).then(() => {
-			assert.deepEqual(sourceMap.sources, ['myFile.ts', 'myFile2.ts']);
+	it('should fix source paths', () => {
+		// Input source map has a source with a webpack path (e.g.,
+		// something!path/to/file.js')
+		return callLoader(getSourceMap()).then(({ sourceMap }) => {
+			assert.sameMembers(sourceMap!.sources, [
+				'myFile1.ts',
+				'myFile2.ts'
+			]);
 		});
 	});
 });
